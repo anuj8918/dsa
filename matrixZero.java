e1ee33af448f5a30ae9fac95a707828aa3ab01d5
Anuj Mishra <anuj.mishra@cron-labs.com>
2026-04-24 14:21:38 +0530

    set matrix 0

---
+++ matrixZero.java
@@ -0,0 +1,48 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int n=matrix.length, m=matrix[0].length;
+        boolean firstRowZero=false, firstColumnZero=false;
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(matrix[0][j]==0){
+                    firstRowZero=true;
+                }
+            }
+        }
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(matrix[i][0]==0){
+                    firstColumnZero=true;
+                }
+            }
+        }
+
+        for(int i=1; i<n; i++){
+            for(int j=1; j<m; j++){
+                if(matrix[i][j]==0){
+                    matrix[0][j]=0;
+                    matrix[i][0]=0;
+                }
+            }
+        }
+        for(int i=1; i<n; i++){
+            for(int j=1; j<m; j++){
+                if(matrix[0][j]==0 || matrix[i][0]==0){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+        if(firstRowZero){
+            for(int j=0; j<m; j++){
+                matrix[0][j]=0;
+            }
+        }
+        if(firstColumnZero){
+            for(int i=0; i<n; i++){
+                matrix[i][0]=0;
+            }
+        }
+    }
+}
