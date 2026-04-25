10227b606775be7a4bcfc7de8f6cf53853e85e1a
anuj8918 <anujm8918@gmail.com>
2026-04-25 22:14:56 +0530

    zig zag optimal

--- zigZagTree.java
+++ zigZagTree.java
@@ -1,68 +1,119 @@
+// /**
+//  * Definition for a binary tree node.
+//  * public class TreeNode {
+//  *     int val;
+//  *     TreeNode left;
+//  *     TreeNode right;
+//  *     TreeNode() {}
+//  *     TreeNode(int val) { this.val = val; }
+//  *     TreeNode(int val, TreeNode left, TreeNode right) {
+//  *         this.val = val;
+//  *         this.left = left;
+//  *         this.right = right;
+//  *     }
+//  * }
+//  */
+// class Solution {
+//     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+//         List<List<Integer>> res = new ArrayList<>();
+
+//         int height = treeHeight(root);
+//         boolean lToRTrav=true;
+
+//         for(int i=1; i<=height; i++){
+//             List<Integer> curr = new ArrayList<>();
+//             if(lToRTrav){
+//                 leftToRightTrav(root, i, curr);
+//             }
+//             else{
+//                 rightToLeftTrav(root, i, curr);
+//             }
+//             res.add(curr);
+//             lToRTrav=!lToRTrav;
+//         }
+//         return res;
+//     }
+//     public int treeHeight(TreeNode root){
+//         if(root==null){
+//             return 0;
+//         }
+//         int lh=treeHeight(root.left);
+//         int rh=treeHeight(root.right);
+//         return Math.max(lh,rh)+1;
+//     }
+//     public void leftToRightTrav(TreeNode root, int level, List<Integer> curr){
+//         if(root==null){
+//             return;
+//         }
+//         if(level==1){
+//             curr.add(root.val);
+//         }
+//         else{
+//             leftToRightTrav(root.left, level-1, curr);
+//             leftToRightTrav(root.right, level-1, curr);
+//         }
+//     }
+//     public void rightToLeftTrav(TreeNode root, int level, List<Integer> curr){
+//         if(root==null){
+//             return;
+//         }
+//         if(level==1){
+//             curr.add(root.val);
+//         }
+//         else{
+//             rightToLeftTrav(root.right, level-1, curr);
+//             rightToLeftTrav(root.left, level-1, curr);
+//         }
+//     }
+// }
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         List<List<Integer>> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        boolean leftToRight = true;
 
-        int height = treeHeight(root);
-        boolean lToRTrav=true;
+        while (!q.isEmpty()) {
+            int size = q.size();
+            List<Integer> level = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = q.poll();
 
-        for(int i=1; i<=height; i++){
-            List<Integer> curr = new ArrayList<>();
-            if(lToRTrav){
-                leftToRightTrav(root, i, curr);
+                if (leftToRight) {
+                    level.addLast(node.val);
+                } else {
+                    level.addFirst(node.val);
+                }
+
+                if (node.left != null) {
+                    q.offer(node.left);
+                }
+                if (node.right != null) {
+                    q.offer(node.right);
+                }
             }
-            else{
-                rightToLeftTrav(root, i, curr);
-            }
-            res.add(curr);
-            lToRTrav=!lToRTrav;
+            res.add(level);
+            leftToRight = !leftToRight;
         }
         return res;
-    }
-    public int treeHeight(TreeNode root){
-        if(root==null){
-            return 0;
-        }
-        int lh=treeHeight(root.left);
-        int rh=treeHeight(root.right);
-        return Math.max(lh,rh)+1;
-    }
-    public void leftToRightTrav(TreeNode root, int level, List<Integer> curr){
-        if(root==null){
-            return;
-        }
-        if(level==1){
-            curr.add(root.val);
-        }
-        else{
-            leftToRightTrav(root.left, level-1, curr);
-            leftToRightTrav(root.right, level-1, curr);
-        }
-    }
-    public void rightToLeftTrav(TreeNode root, int level, List<Integer> curr){
-        if(root==null){
-            return;
-        }
-        if(level==1){
-            curr.add(root.val);
-        }
-        else{
-            rightToLeftTrav(root.right, level-1, curr);
-            rightToLeftTrav(root.left, level-1, curr);
-        }
     }
 }
